f16477ef6c0bb1671d13ef889e9873f8cc5161a8
Pedro Nauck <pedronauck@gmail.com>
2016-09-13 20:09:24 -0300

    refactoring svg utils scritp

--- utils/svg.js
+++ utils/svg.js
@@ -6,30 +6,30 @@ const Svgo = require('svgo');
 const load = (file) =>
   cheerio.load(String(file), { xmlMode: true });
 
-const removeUnnecessaryAttrs = ($, icon) => {
+const removeUnnecessaryAttrs = ($) => {
   const $svg = $('svg');
 
   $svg.children('title').remove();
-  $svg.children('path').removeAttr('fill').removeAttr('fill-rule');
-  $svg.children('g').removeAttr('fill').removeAttr('fill-rule');
+  $svg.children('path').removeAttr('fill');
+  $svg.children('g').removeAttr('fill');
   $svg.removeAttr('xmlns');
 
   return $;
 };
 
-const normalize = (icon, file) => {
-  const $ = removeUnnecessaryAttrs(load(file), icon);
+const normalize = (file) => {
+  const $ = removeUnnecessaryAttrs(load(file));
 
   return _.chain($.html())
+    .replace('fill-rule', 'fillRule')
     .replace('xlink:href', 'xlinkHref')
     .value();
 };
 
-exports.minify = () => through((file, enc, cb) => {
-  new Svgo().optimize(file, result => cb(null, result.data));
-});
+exports.minify = () => through((file, enc, cb) =>
+  new Svgo().optimize(String(file), ({ data }) => cb(null, data)));
 
-exports.parse = (icon) => through((file, enc, cb) =>
-  cb(null, new Buffer(normalize(icon, file), 'utf-8')));
+exports.parse = () => through((file, enc, cb) =>
+  cb(null, new Buffer(normalize(file), 'utf-8')));
 
 exports.load = load;
