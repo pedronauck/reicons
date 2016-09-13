f35a58e63b8cfb2191518814a51efd03a9223fa5
Pedro Nauck <pedronauck@gmail.com>
2016-09-13 15:09:46 -0300

    add a component utils script

---
+++ utils/component.js
@@ -0,0 +1,55 @@
+const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
+const svg = require('./svg');
+const through = require('through2');
+
+const TEMPLATE_DIR = path.resolve(__dirname, '../lib/templates');
+
+const parseName = (prefix, iconName) =>
+  _.upperFirst(_.camelCase(`${prefix}-${iconName}`));
+
+const template = (filename, props) =>
+  _.template(String(fs.readFileSync(`${TEMPLATE_DIR}/${filename}`)))(props);
+
+exports.create = (compName) => through((file, enc, cb) => {
+  const $ = svg.load(file);
+  const svgContent = _.replace($('svg').html(), /"\/>/g, '" />');
+  const defaultSize = $('svg').attr('width');
+  const templateProps = {
+    component: compName,
+    svgContent,
+    defaultSize
+  };
+
+  return cb(null, new Buffer(template('component.js', templateProps), 'utf-8'));
+});
+
+exports.writeJsFile = (compName, build) => through((file, enc, cb) => {
+  const iconPrefix = compName.substring(0, 2);
+  const iconName = compName.substring(2, compName.length);
+  const dirToBundle = `${build}/${iconPrefix}/`;
+
+  const createIconFile = () =>
+    fs.writeFile(`${dirToBundle}/${iconName}.js`, file, 'utf-8', cb);
+
+  const createIconsDir = () => {
+    try {
+      fs.accessSync(dirToBundle);
+      createIconFile();
+    } catch (e) {
+      fs.mkdirSync(dirToBundle);
+      createIconFile();
+    }
+  }
+
+  try {
+    fs.accessSync(build)
+    createIconsDir();
+  } catch (e) {
+    fs.mkdirSync(build);
+    createIconsDir();
+  }
+});
+
+exports.parseName = parseName;
